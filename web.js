49f7db85a46a0fd9342f62092c712662a790160d
Shaun Wilde <shaun_wilde@.rEm0vethIsB1t.hotmail.com>
2013-03-09 08:05:15 +1100

    add mongoose and mongodb hooks

--- web.js
+++ web.js
@@ -1,6 +1,21 @@
+// common settings - use heroku environment variables then fallback to local dev settings
 var port = process.env.PORT || 8888;
-
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/metrics-store'; 
+  
 var restify = require('restify');
+var mongoose = require('mongoose');
+ 
+// prep the mongo database connection
+var mongoOptions = { db: { safe: true }};
+mongoose.connect(mongoUri, mongoOptions, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + mongoUri + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + mongoUri);
+  }
+});
 
 var server = restify.createServer({
   name: 'metrics-store',
